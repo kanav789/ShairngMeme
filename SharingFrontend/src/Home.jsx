c0da140a733c7b29daac3bfe6e6f040017b1fe6f
kanav789 <kkanav73@gmamil.com>
2024-08-01 14:58:18 +0530

    Made a protector in Home file so ,without login no one can check the page

--- SharingFrontend/src/Home.jsx
+++ SharingFrontend/src/Home.jsx
@@ -2,14 +2,20 @@ import React from "react";
 import Navbar from "./UIComponents/Navbar/Navbar";
 import SearchBar from "./UIComponents/SearchBar/SearchBar";
 import { Outlet } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 import "./App.css";
+import LoginForm from "./UIComponents/Register/Login";
 function Home() {
+  const navigate = useNavigate();
+  const authStatus = useSelector((state) => state.auth.status);
   return (
     <div>
       <Navbar />
 
       <SearchBar />
-      <Outlet />
+      {/* adding the Protector */}
+      {authStatus === true ? <Outlet /> : <LoginForm />}
     </div>
   );
 }
